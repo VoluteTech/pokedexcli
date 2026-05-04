c9cd8f009d5b680ab903f44061ffd735e19da8a8
VoluteTech <chewie.louveau@gmail.com>
2026-05-04 15:03:49 +0200

    feat: add cmd handler for exit and help commands

--- repl.go
+++ repl.go
@@ -7,14 +7,34 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			userInput := scanner.Text()
-			clearedInput := cleanInput(userInput)
-			fmt.Printf("Your command was: %v\n", clearedInput[0])
+		scanner.Scan() 
+
+		userInput := cleanInput(scanner.Text())
+		if len(userInput) == 0 {
+			continue
+		}
+
+		commandName := userInput[0]
+		cmd, exists := getCommands()[commandName]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("unknown command")
+			continue
 		}
 	}
 }
@@ -27,4 +47,19 @@ func cleanInput(text string) []string {
 	}
 
 	return parts
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Give you informations about the pokedexcli",
+			callback: commandHelp,
+		},
+	}
 }
