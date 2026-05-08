30c46776c97fec0025e9ad895f265ae991daed4e
VoluteTech <chewie.louveau@gmail.com>
2026-05-08 15:39:04 +0200

    feat: add a getpokemoninfos endpoint

---
+++ internal/api/pokemon.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) GetPokemonInfos(pokemonName string) (Pokemon, error) {
+	url := baseURL + "/pokemon/" + pokemonName
+
+	if val, ok := c.cache.Get(url); ok {
+		var pokemon Pokemon
+		err := json.Unmarshal(val, &pokemon)
+		if err != nil {
+			return Pokemon{}, err
+		}
+		return pokemon, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	defer req.Body.Close()
+	dat, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+	var pokemon Pokemon
+	err = json.Unmarshal(dat, &pokemon)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	c.cache.Add(url, dat)
+
+	return pokemon, nil
+}
